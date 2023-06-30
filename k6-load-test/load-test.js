55e4b87fc726775eae3d0b8eccd7dbcd65f32161
giannoul <giannoul88@gmail.com>
2023-06-30 11:37:59 +0300

    Formatting k6 load test script

--- k6-load-test/load-test.js
+++ k6-load-test/load-test.js
@@ -4,17 +4,17 @@ import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 
 export let options = {
    stages: [
-       { duration: '0.5m', target: 3 }, // simulate ramp-up of traffic from 1 to 3 virtual users over 0.5 minutes.
-       { duration: '0.5m', target: 4}, // stay at 4 virtual users for 0.5 minutes
-       { duration: '0.5m', target: 0 }, // ramp-down to 0 users
+        { duration: '0.5m', target: 3 }, // simulate ramp-up of traffic from 1 to 3 virtual users over 0.5 minutes.
+        { duration: '0.5m', target: 4}, // stay at 4 virtual users for 0.5 minutes
+        { duration: '0.5m', target: 0 }, // ramp-down to 0 users
      ],
 };
 
 export default function () {
    group('foo check', () => {
-       const response = http.get('http://foo.localhost');
-       check(response, {
-           'status code should be 200': res => res.status === 200,
+        const response = http.get('http://foo.localhost');
+        check(response, {
+            'status code should be 200': res => res.status === 200,
        });
    });
 
@@ -29,5 +29,5 @@ export default function () {
 export function handleSummary(data) {
     return {
         'stdout': textSummary(data, { indent: ' ', enableColors: false })
-      };
+    };
 }
